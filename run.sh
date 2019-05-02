aad8d3013441b892386445f053a49725b8753818
sonota88 <yosiot8753@gmail.com>
2019-05-02 10:44:36 +0900

    拡張子を変更: .ob.yaml => .vge.yaml

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -o errexit
 clike_file="$1"
 bname=$(basename $clike_file .vgt.json)
 asm_file="${bname}.vga.yaml"
-bin_file="${bname}.ob.yaml"
+bin_file="${bname}.vge.yaml"
 
 ruby orecc.rb $clike_file > $asm_file
 ruby oreasm.rb $asm_file > $bin_file
