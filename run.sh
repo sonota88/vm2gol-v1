5091392a0248ee75cf773cd374144a0212e11a32
sonota88 <yosiot8753@gmail.com>
2019-05-02 10:45:12 +0900

    rename: bin_file => exe_file (run.sh)

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ set -o errexit
 clike_file="$1"
 bname=$(basename $clike_file .vgt.json)
 asm_file="${bname}.vga.yaml"
-bin_file="${bname}.vge.yaml"
+exe_file="${bname}.vge.yaml"
 
 ruby orecc.rb $clike_file > $asm_file
-ruby oreasm.rb $asm_file > $bin_file
-ruby orevm.rb $bin_file
+ruby oreasm.rb $asm_file > $exe_file
+ruby orevm.rb $exe_file
